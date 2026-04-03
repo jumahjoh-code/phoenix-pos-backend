b88aea7a95fba5d38a036a75b51cd75d2d823099
jumahjoh-code <jumahjoh@gmail.com>
2026-04-03 19:13:09 +0300

    Create token_blacklist.py

---
+++ app/core/token_blacklist.py
@@ -0,0 +1,9 @@
+# app/core/token_blacklist.py
+
+blacklisted_tokens = set()
+
+def blacklist_token(token: str):
+    blacklisted_tokens.add(token)
+
+def is_token_blacklisted(token: str):
+    return token in blacklisted_tokens
